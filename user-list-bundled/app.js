13d6bb20225bb9dd6d0a72bd14a1c7cad4018d82
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2021-04-10 20:16:19 +0530

    config for heroku

--- user-list-bundled/app.js
+++ user-list-bundled/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cors = require('cors');
 const logger = require('morgan');
 const users = require('./routes/users');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
