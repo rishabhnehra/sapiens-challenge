7b4f78001b7cdef7801179c1882831a69498be34
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2021-04-10 19:59:24 +0530

    bundled BE and FE

--- user-list-bundled/app.js
+++ user-list-bundled/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const logger = require('morgan');
 const users = require('./routes/users');
@@ -10,6 +11,7 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/users", users);
 
